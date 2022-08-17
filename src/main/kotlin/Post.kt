94e6fd6b1add48f23811b739af7b07ac89b72cf4
mrIlyasov <mr.ilyasov90@yandex.ru>
2022-08-17 20:52:54 +0500

    Post.kt fix

--- src/main/kotlin/Post.kt
+++ src/main/kotlin/Post.kt
@@ -2,24 +2,11 @@ data class Post(
     val id: Int,
     val from_Id: Int,
     val owner_Id: Int,
-    var text: String,
-    var likes: Likes,
-    var views: Views,
-    var reposts: Reposts,
-    var friendsOnly: Boolean,
-    var markedAsAds: Boolean,
-    ) {
-
-
-    init {
-
-    }
-
-
-
-    override fun toString(): String {
-        return ("$id, $from_Id, $owner_Id, $text, $likes, $views, $reposts, $friendsOnly, $markedAsAds")
-    }
-
-
+    val text: String,
+    val likes: Likes,
+    val views: Views,
+    val reposts: Reposts,
+    val friendsOnly: Boolean,
+    val markedAsAds: Boolean,
+) {
 }
